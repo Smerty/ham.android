2f89979b42a88e7d3c6c2512f9d1ac7b7f1f32aa
Paul Picazo <ppicazo@gmail.com>
2010-08-15 23:17:25 -0700

    do the download in another thread and put up progress dialog

--- src/com/smerty/ham/QRZ.java
+++ src/com/smerty/ham/QRZ.java
@@ -4,10 +4,14 @@ import org.smerty.ham.qrz.QrzDb;
 import org.smerty.ham.qrz.QrzHamProfile;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.AsyncTask.Status;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
@@ -19,6 +23,9 @@ import android.widget.TextView;
 public class QRZ extends Activity {
 	
 	public static final String PREFS_NAME = "HamPrefs";
+	
+	private AsyncTask<QRZ, Integer, QrzHamProfile> updatetask;
+	public ProgressDialog progressDialog;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -55,40 +62,108 @@ public class QRZ extends Activity {
 					return;
 				}
 				
-				TextView nameText = (TextView) findViewById(R.id.TextProfileName);
-				TextView emailText = (TextView) findViewById(R.id.TextProfileEmail);
-				TextView classText = (TextView) findViewById(R.id.TextProfileClass);
-				TextView expiresText = (TextView) findViewById(R.id.TextProfileExpDate);
-				
-				
-				
-				TextView callText = (TextView) findViewById(R.id.TextView01);
-				TextView stateText = (TextView) findViewById(R.id.TextView02);
-				TextView countryText = (TextView) findViewById(R.id.TextView03);
-				TextView gridText = (TextView) findViewById(R.id.TextView04);
-				
-				try {
-					QrzDb qrzDb = new QrzDb(qrzUser, qrzPassword);
-					
-					QrzHamProfile profile = qrzDb.getHamByCallsign(callsignSearchText.getText().toString());
-					
-					nameText.setText(profile.getFname() + " " + profile.getName());
-					callText.setText("Callsign: " + profile.getCall());
-					stateText.setText("State: " + profile.getState());
-					emailText.setText("Email: " + profile.getEmail());
-					classText.setText("Class: " + profile.getHamclass());
-					expiresText.setText("Expires: " + profile.getExpdate());
-					countryText.setText("Country: " + profile.getCountry());
-					gridText.setText("Grid: " + profile.getGrid());
-					
-					LinearLayout resultsLL = (LinearLayout) findViewById(R.id.ResultsLL01);
-					resultsLL.setVisibility(View.VISIBLE);
-					
-				} catch (Exception e) {
-					e.printStackTrace();
+				if (QRZ.this.updatetask == null) {
+					Log.d("startDownloading", "task was null, calling execute");
+					QRZ.this.updatetask = new GetProfileTask().execute(QRZ.this);
+				} else {
+					Status s = QRZ.this.updatetask.getStatus();
+					if (s == Status.FINISHED) {
+						Log
+								.d("updatetask",
+										"task wasn't null, status finished, calling execute");
+						QRZ.this.updatetask = new GetProfileTask().execute(QRZ.this);
+					}
 				}
+				
+				
 				
 			}
 		});
+	}
+	
+	private class GetProfileTask extends AsyncTask<QRZ, Integer, QrzHamProfile> {
+
+		QRZ that;
+
+		protected QrzHamProfile doInBackground(QRZ... thats) {
+
+			if (that == null) {
+				this.that = thats[0];
+			}
+
+			QrzHamProfile profile = null;
+			
+			publishProgress(0);
+			
+			
+			
+			try {
+				SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+				final String qrzUser = settings.getString("qrzUser", null);
+				final String qrzPassword = settings.getString("qrzPassword", null);
+				
+				QrzDb qrzDb = new QrzDb(qrzUser, qrzPassword);
+				
+				final EditText callsignSearchText = (EditText) findViewById(R.id.EditText01);
+				
+				profile = qrzDb.getHamByCallsign(callsignSearchText.getText().toString());
+				
+				
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+
+			publishProgress(100);
+
+			return profile;
+		}
+
+		protected void onProgressUpdate(Integer... progress) {
+			Log.d("onProgressUpdate", progress[0].toString());
+			if (progress[0] == 0) {
+				that.progressDialog = ProgressDialog.show(that, "Ham",
+						"Querying QRZ.com API", true, false);
+			}
+			if (progress[0] == 100) {
+				that.progressDialog.dismiss();
+			}
+
+		}
+
+		protected void onPostExecute(QrzHamProfile result) {
+			//Log.d("onPostExecute", that.getApplicationInfo().packageName);
+			//that.allTogether();
+			
+			if (result == null) {
+				return;
+			}
+			
+			TextView nameText = (TextView) findViewById(R.id.TextProfileName);
+			TextView emailText = (TextView) findViewById(R.id.TextProfileEmail);
+			TextView classText = (TextView) findViewById(R.id.TextProfileClass);
+			TextView expiresText = (TextView) findViewById(R.id.TextProfileExpDate);
+			
+			
+			
+			TextView callText = (TextView) findViewById(R.id.TextView01);
+			TextView stateText = (TextView) findViewById(R.id.TextView02);
+			TextView countryText = (TextView) findViewById(R.id.TextView03);
+			TextView gridText = (TextView) findViewById(R.id.TextView04);
+			
+			nameText.setText(result.getFname() + " " + result.getName());
+			callText.setText("Callsign: " + result.getCall());
+			stateText.setText("State: " + result.getState());
+			emailText.setText("Email: " + result.getEmail());
+			classText.setText("Class: " + result.getHamclass());
+			expiresText.setText("Expires: " + result.getExpdate());
+			countryText.setText("Country: " + result.getCountry());
+			gridText.setText("Grid: " + result.getGrid());
+			
+			LinearLayout resultsLL = (LinearLayout) findViewById(R.id.ResultsLL01);
+			resultsLL.setVisibility(View.VISIBLE);
+			
+		}
 	}
 }
