06da28252bd82f7cea4d26df911768f2c970afd9
Paul Picazo <ppicazo@gmail.com>
2011-04-24 03:16:36 -0700

    Eliminate some warnings

--- src/org/smerty/ham/qrz/QrzDbCallsignException.java
+++ src/org/smerty/ham/qrz/QrzDbCallsignException.java
@@ -1,6 +1,9 @@
 package org.smerty.ham.qrz;
 
 public class QrzDbCallsignException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
 	public QrzDbCallsignException(String message) {
 		// TODO Auto-generated constructor stub
 		super(message);
--- src/org/smerty/ham/qrz/QrzDbSessionException.java
+++ src/org/smerty/ham/qrz/QrzDbSessionException.java
@@ -1,6 +1,9 @@
 package org.smerty.ham.qrz;
 
 public class QrzDbSessionException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
 	public QrzDbSessionException(String message) {
 		// TODO Auto-generated constructor stub
 		super(message);
